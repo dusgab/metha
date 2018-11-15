1267ce9c46ce0b9edb43b3c91736f3432c20120d
Dustin Gassmann <dustingassmann@gmail.com>
2018-11-15 10:30:04 -0300

    fix minor

--- config/mail.php
+++ config/mail.php
@@ -4,7 +4,7 @@ return [
     'driver' => env('MAIL_DRIVER'),
     'host' => env('MAIL_HOST'),
     'port' => env('MAIL_PORT'),
-    'from' => ['address' => 'METHA WEB', 'name' => 'METHA WEB'],
+    'from' => ['address' => 'administrador@metha.gob.ar', 'name' => 'METHA WEB'],
     'encryption' => env('MAIL_ENCRYPTION'),
     'username' => env('MAIL_USERNAME'),
     'password' => env('MAIL_PASSWORD'),
