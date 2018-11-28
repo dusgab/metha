09b3572fe6a8c84848bae80edec8ab6012e5fe90
Dustin Gassmann <dustingassmann@gmail.com>
2018-11-28 12:45:04 -0300

    cambio de posiciones de grid

--- resources/views/precios.blade.php
+++ resources/views/precios.blade.php
@@ -9,73 +9,6 @@
             <h1 class="text-center">Precios <small>Referencia de los distintos precios ofrecidos, demandados y de otros mercados</small></h1></div>
         <div class="row precios">
             <div class="col-md-6">
-                <h1 class="h1-tabla">Principales Mercados</h1>
-                <h5 class="text-center">Enlaces a los Sitios de los Principales Mercados de Argentina y la Región</h5>
-                <div class="table-responsive admin">
-                    <table class="table enlaces">
-                        <thead>
-                            <tr>
-                                <th class="enlaces"><a href="http://www.mercadocentral.gob.ar/servicios/precios-y-volumenes/precios-mayoristas" class="links"> Mercado de Buenos Aires </a></th>
-                            </tr>
-                            <tr>
-                                <th class="enlaces"><a href="https://mercacordoba.es/mercado-de-frutas-y-verduras">Mercado de Córdoba </a></th>
-                            </tr>
-                            <tr>
-                                <th class="enlaces"><a href="http://www.mprosario.com.ar/">Mercado de Rosario</a></th>
-                            </tr>
-                            <tr>
-                                <th class="enlaces"><a href="http://www.cooperativah.com.ar/">Cooperativa de Horticultores de Bahía Blanca</a></th>
-                            </tr>
-                            <tr>
-                                <th class="enlaces"><a href="http://mercadoproductoressantafe.com/">Mercado de Santa Fé</a></th>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <h1 class="h1-tabla">Precios del Día</h1>
-                <h5 class="text-center">Seleccione un día para ver los precios de cada Producto</h5>
-                <div class="row">
-                <div id="imaginary_container"> 
-                    <form class="form-horizontal" method="GET" action="{{ url('/precios') }}">
-                        <div class="col-md-6 fecha">
-                        <input type="text" class="form-control fecha" name="buscar" placeholder="Buscar Producto" onfocus="this.placeholder=''" onblur="this.placeholder='Buscar Producto'">
-                        <input placeholder="Seleccione un Día" onfocus="(this.type='date')" class="form-control fecha" type="text" class="form-control precioDia" onblur="if(this.value==''){this.type='text'}" name="precioDia" value="">
-                        <button type="submit" class="lupa" id="lupa">
-                            <span type"submit" class="glyphicon glyphicon-search lupa"></span>
-                        </button>
-                        </div>
-                    </form>
-                </div>
-                </div>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Producto </th>
-                                <th>Min </th>
-                                <th>Medio </th>
-                                <th>Max </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($preciosd as $pr)
-                            <tr>
-                                <td>{{$pr->nombre}} </td>
-                                <td>$ {{$pr->min}} </td>
-                                <td>$ {{$pr->prom}} </td>
-                                <td>$ {{$pr->max}} </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {!! $preciosd->appends(array_except(Request::query(), 'pd'))->links() !!}
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
                 <h1 class="h1-tabla">Precios Ofrecidos</h1>
                 <h5 class="text-center">Seleccione un intervalo de fechas para filtrar los Precios de los Productos</h5>
                 <div class="row">
@@ -121,6 +54,73 @@
                         </tbody>
                     </table>
                     {!! $precioso->appends(array_except(Request::query(), 'po'))->links() !!}
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h1 class="h1-tabla">Precios del Día</h1>
+                <h5 class="text-center">Seleccione un día para ver los precios de cada Producto</h5>
+                <div class="row">
+                <div id="imaginary_container"> 
+                    <form class="form-horizontal" method="GET" action="{{ url('/precios') }}">
+                        <div class="col-md-6 fecha">
+                        <input type="text" class="form-control fecha" name="buscar" placeholder="Buscar Producto" onfocus="this.placeholder=''" onblur="this.placeholder='Buscar Producto'">
+                        <input placeholder="Seleccione un Día" onfocus="(this.type='date')" class="form-control fecha" type="text" class="form-control precioDia" onblur="if(this.value==''){this.type='text'}" name="precioDia" value="">
+                        <button type="submit" class="lupa" id="lupa">
+                            <span type"submit" class="glyphicon glyphicon-search lupa"></span>
+                        </button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Producto </th>
+                                <th>Min </th>
+                                <th>Medio </th>
+                                <th>Max </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($preciosd as $pr)
+                            <tr>
+                                <td>{{$pr->nombre}} </td>
+                                <td>$ {{$pr->min}} </td>
+                                <td>$ {{$pr->prom}} </td>
+                                <td>$ {{$pr->max}} </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {!! $preciosd->appends(array_except(Request::query(), 'pd'))->links() !!}
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h1 class="h1-tabla">Principales Mercados</h1>
+                <h5 class="text-center">Enlaces a los Sitios de los Principales Mercados de Argentina y la Región</h5>
+                <div class="table-responsive admin">
+                    <table class="table enlaces">
+                        <thead>
+                            <tr>
+                                <th class="enlaces"><a href="http://www.mercadocentral.gob.ar/servicios/precios-y-volumenes/precios-mayoristas" class="links"> Mercado de Buenos Aires </a></th>
+                            </tr>
+                            <tr>
+                                <th class="enlaces"><a href="https://mercacordoba.es/mercado-de-frutas-y-verduras">Mercado de Córdoba </a></th>
+                            </tr>
+                            <tr>
+                                <th class="enlaces"><a href="http://www.mprosario.com.ar/">Mercado de Rosario</a></th>
+                            </tr>
+                            <tr>
+                                <th class="enlaces"><a href="http://www.cooperativah.com.ar/">Cooperativa de Horticultores de Bahía Blanca</a></th>
+                            </tr>
+                            <tr>
+                                <th class="enlaces"><a href="http://mercadoproductoressantafe.com/">Mercado de Santa Fé</a></th>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <div class="col-md-6">
